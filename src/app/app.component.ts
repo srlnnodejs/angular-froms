73f2c8dceebe4c3f9f7b3b84b45eb156573ce2db
srlnnodejs <srlnnodejs@gmail.com>
2019-12-14 22:27:59 +0530

    first step angular form completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import {User} from "./user"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+topics=["angular","react","vue"];
+userModel=new User("rob","rob@test.com",9861152315,'','morning',true)
 }
